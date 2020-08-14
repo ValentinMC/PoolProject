7b089fdc183a20c585e71ac8cc16ac85b859c800
ValentinMC <valentincalin.99@gmail.com>
2020-08-14 21:10:41 +0200

    Errores PHP en puerto 35 por el SMPT

--- PHP/prueba1.php
+++ PHP/prueba1.php
@@ -1,4 +1,7 @@
 <?php
+//Hay que verificar primero que el puerto 25 del SMPT no esta ocupado
+//u obstruido por el firewall
+if(isset($_POST['submit'])){
   $nombre = $_POST['nombre'];
   $apellido = $_POST['apellido'];
   $dni = $_POST['dni'];
@@ -6,15 +9,16 @@
   $email = $_POST['email'];
   $tlf = $_POST['tlf'];
 
-  $email_from = "$email";
+  //$email_from = $email;
 
 	$email_subject = "New Form submission";
 
 	$email_body = "You have received a new message from the user $nombre.\n".
-                            "Here is the message:\n $tlf".
+                            "Here is the message: $tlf".
   $to = "valentincalin.99@gmail.com";
 
-   $headers = "From: $email_from \r\n";
+   $headers = "From: $email \r\n";
 
    mail($to,$email_subject,$email_body,$headers);
+}
 ?>
