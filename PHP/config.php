a55bc65855737c1ce397ef6123aa9df79cad7966
ValentinMC <valentincalin.99@gmail.com>
2020-08-20 16:34:27 +0200

    Registro y conexión a BBDD conseguida

--- PHP/config.php
+++ PHP/config.php
@@ -5,7 +5,7 @@ define('HOST', 'localhost');
 define('DATABASE', 'pool_users');
 
 try {
-    $connection = new PDO("mysql:host=".HOST.";dbname=".DATABASE, USER, PASSWORD);
+    $connection = new PDO('mysql:host='.HOST.';dbname='.DATABASE, USER, PASSWORD);
 } catch (PDOException $e) {
     exit("Error: " . $e->getMessage());
 }
